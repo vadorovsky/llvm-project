4f104a5541cf398ccca5ce5c3e1bdfe79d0590c2
Davide Bertola <dade@dadeb.it>
2022-11-05 17:06:58 +0100

    add LLVMGetMDNodeTag

--- llvm/tools/llvm-c-test/main.c
+++ llvm/tools/llvm-c-test/main.c
@@ -49,6 +49,8 @@ static void print_usage(void) {
   fprintf(stderr, "  * --is-a-value-as-metadata\n");
   fprintf(stderr,
           "    Run test for checking if LLVMValueRef is a ValueAsMetadata\n");
+  fprintf(stderr, "  * --get-di-tag\n");
+  fprintf(stderr, "    Run test for getting MDNode dwarf tag\n");
   fprintf(
       stderr,
       "    Read lines of name, rpn from stdin - print generated module\n\n");
@@ -100,6 +102,8 @@ int main(int argc, char **argv) {
     return llvm_replace_md_operand();
   } else if (argc == 2 && !strcmp(argv[1], "--is-a-value-as-metadata")) {
     return llvm_is_a_value_as_metadata();
+  } else if (argc == 2 && !strcmp(argv[1], "--get-di-tag")) {
+    return llvm_get_di_tag();
   } else if (argc == 2 && !strcmp(argv[1], "--test-function-attributes")) {
     return llvm_test_function_attributes();
   } else if (argc == 2 && !strcmp(argv[1], "--test-callsite-attributes")) {
